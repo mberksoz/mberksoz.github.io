84557b22849dcc36f483256c7e14cfae996ee044
mberksoz <95355405+mberksoz@users.noreply.github.com>
2025-02-02 16:23:25 +0000

    Deploying to gh-pages from @ mberksoz/mberksoz.github.io@1c0cd3ffd599a2feda822d22adb82a27bb0f34ce 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-",
-          title: "",
+        },{id: "nav-cv",
+          title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
